fec56dddbe205fa7b27d7dd030fbc5783beed038
liloudantin <lilou.dantin@gmail.com>
2024-04-13 23:30:22 +0200

    13042024LILOU

--- Assets/Scripts/Play_button.cs
+++ Assets/Scripts/Play_button.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.IO;
+
 
 public class Play_button : MonoBehaviour
 {
@@ -12,6 +15,10 @@ public class Play_button : MonoBehaviour
     public Image netImage;
     public Button[] Regles_nv_buttons;
     public static int niveau = 6;
+    public int nbofQuestion = 0;
+
+    public int totalCorrectAnswers = 0;
+    private List<string[]> csvData = new List<string[]>();
 
     void Start()
     {
@@ -157,6 +164,9 @@ public class Play_button : MonoBehaviour
         {
             button.onClick.AddListener(OnReglesNvButtonClick);
         }
+
+        // Charger le CSV
+        LoadCSV(@"C:\Users\lilou\Documents\CSNumVi\Impliquer\VolleyMaster\Assets\Regles_formatclassique.csv");
     }
 
     void OnPlayButtonClick()
@@ -181,6 +191,32 @@ public class Play_button : MonoBehaviour
 
     }
 
+    void LoadCSV(string fileName)
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+
+        if (File.Exists(filePath))
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                // Ignorer la première ligne (noms de colonnes)
+                reader.ReadLine();
+
+                // Lire le reste du fichier
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(',');
+                    csvData.Add(values);
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("CSV file not found at: " + filePath);
+        }
+    }
+
     // Fonction d'écouteur de clic pour le bouton "Règles"
     void OnReglesButtonClick()
     {
@@ -195,11 +231,74 @@ public class Play_button : MonoBehaviour
             techniqueButton.gameObject.SetActive(false);
 
        // Activer les boutons de niveau
-        foreach(Button button in Regles_nv_buttons)
+        //foreach(Button button in Regles_nv_buttons)
+        //{
+         //   button.gameObject.SetActive(true);
+        //}
+        
+        // Désactiver les boutons de niveau pour les niveaux avec un "totbonnesrep" supérieur à 3
+        DisableButtonsForHighTotbonnesrep();
+
+        int niveau = Play_button.niveau; // Niveau spécifié
+    }
+
+    void DisableButtonsForHighTotbonnesrep()
+    {
+        // Parcourir les données CSV pour vérifier les "totbonnesrep" pour chaque niveau
+        for (int i = 0; i < 6; i++) // Supposant que vous avez 6 niveaux
         {
-            button.gameObject.SetActive(true);
+            int niveauToCheck = i + 1; // Niveau commence à partir de 1
+            int totalCorrectAnswers = GetTotalCorrectAnswersForLevel(niveauToCheck);
+    
+            // Si le total des bonnes réponses dépasse 3, désactiver le bouton de niveau correspondant
+            if (totalCorrectAnswers < (3 * nbofQuestion))
+            {
+                Regles_nv_buttons[i].gameObject.SetActive(true);
+            }
+            else
+            {   
+                // Rendre le bouton non cliquable et le griser
+                Regles_nv_buttons[i].gameObject.SetActive(true);
+                Regles_nv_buttons[i].interactable = false;
+                Color disabledColor = new Color(0.7f, 0.7f, 0.7f, 1f); // Couleur grise
+                Regles_nv_buttons[i].image.color = disabledColor; // Changer la couleur de l'image
+            }
         }
     }
+
+
+    int GetTotalCorrectAnswersForLevel(int niveauToCheck)
+    {
+        totalCorrectAnswers = 0;
+        nbofQuestion = 0;
+        // Filtrer les lignes du CSV pour le niveau spécifié
+        List<string[]> filteredData = csvData.FindAll(line =>
+        {
+            int parsedLevel;
+            if (int.TryParse(line[6], out parsedLevel))
+            {
+                return parsedLevel == niveauToCheck;
+            }
+            else
+            {
+                return false;
+            }
+        });
+
+        // Calculer le nombre total de bonnes réponses pour ce niveau
+        foreach (var line in filteredData)
+        {
+            int questionTotalCorrectAnswers;
+            if (int.TryParse(line[7], out questionTotalCorrectAnswers))
+            {
+                totalCorrectAnswers += questionTotalCorrectAnswers;
+                nbofQuestion++;
+            }
+        }
+
+        return totalCorrectAnswers;
+    }
+
 
     public void OnReglesNvButtonClick()
     {
--- Assets/Scripts/Validate_button.cs
+++ Assets/Scripts/Validate_button.cs
@@ -25,7 +25,11 @@ public class Validate_button : MonoBehaviour
     public Button Answer_3;
     public Button Answer_4;
 
-    public Image netImage; // Ajoutez cette ligne pour référencer l'image "Net"
+    public Image netImage; 
+
+    public Button Logo;
+
+    public Button ReturnButton;
 
 
     void Start()
@@ -40,11 +44,16 @@ public class Validate_button : MonoBehaviour
 
         correctionButton.GetComponent<Button>().onClick.AddListener(DisplayNewQuestion); // Attach DisplayNewQuestion to the click event of the correction button
 
-        netImage.GetComponent<Button>().onClick.AddListener(ReturnToMenu); // Ajoutez cette ligne pour gérer le clic sur l'image "Net"
-
         // Charger le CSV et afficher la première question
         LoadCSV(@"C:\Users\lilou\Documents\CSNumVi\Impliquer\VolleyMaster\Assets\Regles_formatclassique.csv");
         DisplayRandomQuestion();
+
+        // Récupère le bouton "Logo" et ajoute un écouteur pour son clic
+        Button logoButton = GameObject.Find("Logo").GetComponent<Button>();
+        logoButton.onClick.AddListener(RetournerAuMenu);
+
+        // Ajouter un écouteur de clic pour le bouton "Return_button"
+        ReturnButton.onClick.AddListener(RetournerAuMenu);
     }
 
     void LoadCSV(string fileName)
@@ -86,7 +95,7 @@ public class Validate_button : MonoBehaviour
             int parsedLevel;
             if (int.TryParse(line[6], out parsedLevel))
             {
-                return parsedLevel == niveau;
+                return parsedLevel == niveau && int.Parse(line[7]) <= 3;
             }
             else
             {
@@ -97,6 +106,40 @@ public class Validate_button : MonoBehaviour
         if (filteredData.Count == 0)
         {
             Debug.LogWarning("Aucune question trouvée pour le niveau " + niveau);
+            // Afficher un message indiquant que toutes les questions du niveau ont été terminées
+            QuestionText.text = "Bravo ! Vous avez fini toutes les questions de ce niveau";
+            // Cacher les boutons de réponse et le bouton de validation
+            Answer_1.gameObject.SetActive(false);
+            Answer_2.gameObject.SetActive(false);
+            Answer_3.gameObject.SetActive(false);
+            Answer_4.gameObject.SetActive(false);
+            validationButton.SetActive(false);
+            ReturnButton.gameObject.SetActive(true);
+            return;
+        }
+
+        // Vérifier si toutes les questions du niveau ont été répondues correctement
+        int totalCorrectAnswers = 0;
+        foreach (var line in filteredData)
+        {
+            int questionTotalCorrectAnswers = int.Parse(line[7]);
+            if (questionTotalCorrectAnswers > 3)
+            {
+                totalCorrectAnswers++;
+            }
+        }
+
+        if (totalCorrectAnswers == filteredData.Count)
+        {
+            // Afficher un message indiquant que toutes les questions du niveau ont été terminées
+            QuestionText.text = "Bravo ! Vous avez fini toutes les questions de ce niveau";
+            // Cacher les boutons de réponse et le bouton de validation
+            Answer_1.gameObject.SetActive(false);
+            Answer_2.gameObject.SetActive(false);
+            Answer_3.gameObject.SetActive(false);
+            Answer_4.gameObject.SetActive(false);
+            validationButton.SetActive(false);
+            ReturnButton.gameObject.SetActive(true);
             return;
         }
 
@@ -106,6 +149,9 @@ public class Validate_button : MonoBehaviour
 
         // Sauvegarder l'index de la bonne réponse
         bonneReponseIndex = int.Parse(randomLine[5]);
+
+        // Stocker le total des bonnes réponses pour cette question spécifique
+        totbonnesrep = int.Parse(randomLine[7]); // Assumant que le total des bonnes réponses est à l'index 7
 
         // Afficher la question
         QuestionText.text = randomLine[0];
@@ -118,10 +164,10 @@ public class Validate_button : MonoBehaviour
 
         Debug.Log("L'index de la bonne réponse est : " + bonneReponseIndex); // Ajout du message de débogage
 
-
         // Afficher le bouton de validation
         validationButton.SetActive(true);
     }
+
 
     public void ClickonAnswer(int selectedIndex)
     {
@@ -134,30 +180,165 @@ public class Validate_button : MonoBehaviour
 
         if (selectedIndex == bonneReponseIndex)
         {
-            // Si la réponse est correcte, incrémentez le compteur de bonnes réponses
+            // Réponse correcte
+
+            // Si la réponse est correcte, incrémentez le score total des bonnes réponses
             totbonnesrep++;
+
             correctionText.text = "Bravo !";
             Debug.Log("Bonne réponse ! Nombre total de bonnes réponses : " + totbonnesrep);
             correctionButton.GetComponent<Image>().color = Color.green; // Change la couleur du bouton correctionButton en vert
+
+            // Afficher la bonne réponse en vert
+            switch (bonneReponseIndex)
+            {
+                case 1:
+                    Answer_1.GetComponent<Image>().color = Color.green;
+                    break;
+                case 2:
+                    Answer_2.GetComponent<Image>().color = Color.green;
+                    break;
+                case 3:
+                    Answer_3.GetComponent<Image>().color = Color.green;
+                    break;
+                case 4:
+                    Answer_4.GetComponent<Image>().color = Color.green;
+                    break;
+            }
         }
         else
         {
+            // Réponse incorrecte
             correctionText.text = "Dommage...";
             Debug.Log("Mauvaise réponse !");
+            // Change la couleur du bouton cliqué en rouge
+            switch (selectedIndex)
+            {
+                case 1:
+                    Answer_1.GetComponent<Image>().color = Color.red;
+                    break;
+                case 2:
+                    Answer_2.GetComponent<Image>().color = Color.red;
+                    break;
+                case 3:
+                    Answer_3.GetComponent<Image>().color = Color.red;
+                    break;
+                case 4:
+                    Answer_4.GetComponent<Image>().color = Color.red;
+                    break;
+            }
+            // Afficher la bonne réponse en vert
+            switch (bonneReponseIndex)
+            {
+                case 1:
+                    Answer_1.GetComponent<Image>().color = Color.green;
+                    break;
+                case 2:
+                    Answer_2.GetComponent<Image>().color = Color.green;
+                    break;
+                case 3:
+                    Answer_3.GetComponent<Image>().color = Color.green;
+                    break;
+                case 4:
+                    Answer_4.GetComponent<Image>().color = Color.green;
+                    break;
+            }
             correctionButton.GetComponent<Image>().color = Color.red; // Change la couleur du bouton correctionButton en rouge
         }
 
         correctionButton.SetActive(true); // Afficher le bouton Correction
+        
+        // Enregistrer les données mises à jour dans le fichier CSV
+        SaveCSV();
+
     }
+
 
     public void DisplayNewQuestion()
     {
+        // Réinitialiser la couleur des boutons à leur couleur normale
+        ResetButtonColors();
+
         correctionButton.SetActive(false); // Cacher le bouton Correction
         DisplayRandomQuestion(); // Afficher une nouvelle question
     }
 
-    public void ReturnToMenu()
+    private void ResetButtonColors()
     {
-        SceneManager.LoadScene("Menu"); 
+        // Réinitialiser la couleur de tous les boutons à leur couleur normale
+        Answer_1.GetComponent<Image>().color = Color.white;
+        Answer_2.GetComponent<Image>().color = Color.white;
+        Answer_3.GetComponent<Image>().color = Color.white;
+        Answer_4.GetComponent<Image>().color = Color.white;
     }
+
+    void RetournerAuMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        LoadPlayButtonScript();
+    }
+
+    void LoadPlayButtonScript()
+    {
+        GameObject playButtonObject = GameObject.Find("Play_button"); // Assurez-vous que le nom est correct
+        if (playButtonObject != null)
+        {
+            Play_button playButtonScript = playButtonObject.GetComponent<Play_button>();
+            if (playButtonScript != null)
+            {
+                // Chargez votre script Play_button ici
+            }
+            else
+            {
+                Debug.LogWarning("Script Play_button introuvable sur l'objet Play_button.");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Objet Play_button introuvable dans la scène.");
+        }
+    }
+
+    void SaveCSV()
+    {
+        string filePath = @"C:\Users\lilou\Documents\CSNumVi\Impliquer\VolleyMaster\Assets\Regles_formatclassique.csv";
+
+        // Vérifier si le fichier existe
+        if (!File.Exists(filePath))
+        {
+            Debug.LogError("Fichier CSV non trouvé à l'emplacement: " + filePath);
+            return;
+        }
+
+        // Parcourir chaque ligne du fichier CSV
+        for (int i = 0; i < csvData.Count; i++)
+        {
+            // Vérifier si la question correspond à celle actuellement affichée
+            if (csvData[i][0] == QuestionText.text) // Assumant que la question est dans la première colonne
+            {
+                // Mettre à jour le total des bonnes réponses pour cette question spécifique
+                int currentTotal = int.Parse(csvData[i][7]); // Récupérer le total actuel des bonnes réponses
+                currentTotal++; // Incrémenter le total
+                csvData[i][7] = currentTotal.ToString(); // Mettre à jour le total des bonnes réponses dans la colonne correspondante
+
+                // Réécrire le fichier CSV avec les données mises à jour
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (string[] rowData in csvData)
+                    {
+                        writer.WriteLine(string.Join(",", rowData));
+                    }
+                }
+
+                Debug.Log("Données sauvegardées dans le fichier CSV avec succès.");
+                return; // Sortir de la boucle une fois que la question a été trouvée et mise à jour
+            }
+        }
+
+        // Afficher un message d'erreur si la question actuelle n'a pas été trouvée dans le fichier CSV
+        Debug.LogError("La question actuelle n'a pas été trouvée dans le fichier CSV.");
+    }
+
+
+
 }
